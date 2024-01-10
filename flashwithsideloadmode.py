8b901c8c1cbf77e46b1980ac9374c006d3ab331b
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-01-10 11:07:31 +0600

    Update flashwithsideloadmode.py

--- flashwithsideloadmode.py
+++ flashwithsideloadmode.py
@@ -1,57 +1,41 @@
 import os
-import sys
+import zipfile
+
+print("\nFlashing Custom Recovery...\n")
+
+C_FOLDER = "/sdcard/Download/mi-flash-CustomRecovery"
+
+if not os.path.exists(C_FOLDER):
+    os.makedirs(C_FOLDER)
+
+input("\nMake sure to place the recovery file in the {} folder, then press Enter\n".format(C_FOLDER))
 
 while True:
-    if len(sys.argv) < 2:
-        target_filename = input("\n\033[92mEnter target name\033[0m: ")
-    else:
-        target_filename = sys.argv[1]
+    img_files = [f for f in os.listdir(C_FOLDER) if f.endswith(".img")]
+    zip_files = [f for f in os.listdir(C_FOLDER) if f.endswith(".zip")]
 
-    if target_filename:
-        break 
-
-file_paths = []
-for root, dirs, files in os.walk("/sdcard"):
-    if target_filename in files:
-        file_paths.append(os.path.join(root, target_filename))
-
-if file_paths:
-    for i, file in enumerate(file_paths, start=1):
-        print(f"{i}. {file}")
-
-    while True:
-        try:
-            selected_index = int(input("\nEnter the number corresponding to the correct file For confirmation: "))
-            if 1 <= selected_index <= len(file_paths):
-                break
-            else:
-                print("\nInvalid selection. Please enter a valid number\n")
-        except ValueError:
-            print("\nInvalid input. Please enter a valid number\n")
-
-    selected_file = file_paths[selected_index - 1]
-    print(f"\nSelected file '{selected_file}'\n")
-else:
-    print(f"file {file_paths} not found")
-    exit()
-
-zipfile = selected_file.endswith(".zip")
-
-if zipfile:
-    print(f"\nfile {selected_file} {zipfile} is zip 'ok'\n")
-else:
-    print(f"\nfile {selected_file} {zipfile} is not zip 'exit'\n")
-    exit(1)
-
-input("\nMake sure your device is in sideload mode. Connect your device using OTG, then press Enter when ready\n")
-
-while True:
-    status = os.popen("adb get-state").read().strip()
-    print(status)
-    if status == "sideload":
+    if img_files:
+        break
+    elif zip_files:
+        zip_file = os.path.join(C_FOLDER, zip_files[0])
+        recovery_img = "recovery.img"
+        
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            zip_ref.extract(recovery_img, C_FOLDER)
+        
         break
     else:
-        input("\nplease Verify that device is in sideload mode ! If so, check that it is connected via otg ! then press Enter\n")
+        input("\nPlease make sure to place the recovery file in the {} folder, then press Enter\n".format(C_FOLDER))
         continue
 
-os.system(f"adb sideload {selected_file}")
+input("\nMake sure your device is in fastboot mode. Connect your device using OTG, then press Enter when ready\n")
+
+while True:
+    status = os.popen("fastboot devices | grep -o 'fastboot'").read().strip()
+    if status == "fastboot":
+        break
+    else:
+        input("\nVerify that the device is in fastboot mode! If so, check that it is connected via OTG! Then press Enter\n")
+        continue
+
+os.system("fastboot flash recovery {}/{}".format(C_FOLDER, [f for f in os.listdir(C_FOLDER) if f.endswith(".img")][0]))
